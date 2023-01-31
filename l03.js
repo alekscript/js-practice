b1d79974601f3eb564bdf4ac03965de66b673116
alekscript <piatopal.alex@gmail.com>
2023-01-31 20:08:55 +0200

    lesson command alert,prompt,confirm

---
+++ l03.js
@@ -0,0 +1,8 @@
+// command (alert, prompt, confirm)
+alert("Hello"); // alert - modal window with parametr OK
+
+result = prompt(title, [def]); // prompt - modal window with 2 parametrs OK-CANCEL
+
+let isBoss = confirm("Ты здесь главный?");
+
+alert( isBoss ); // выпадает окно с вопросом, если нажат ОК - прилетает true
