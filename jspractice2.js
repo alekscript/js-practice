78a1172e14f1d6051182bc24baf0cd1105c99d41
alekscript <piatopal.alex@gmail.com>
2023-02-05 21:48:48 +0200

    task dog years update code

--- jspractice2.js
+++ jspractice2.js
@@ -18,7 +18,7 @@ let humanYearsCatYearsDogYears = function(humanYears) {
   let catYears;
   let dogYears;
   
-  for ( let i = 0; i < humanYears; i ++) {
+ 
     if (humanYears == 1) {
       catYears = 15;
       dogYears = 15;
@@ -26,10 +26,9 @@ let humanYearsCatYearsDogYears = function(humanYears) {
       catYears = 24;
       dogYears = 24;
     } else {
-      catYears = 24 + ((i-1) * 4 );
-      dogYears = 24 + ((i-1) * 5 );
+      catYears = 24 + ((humanYears-2) * 4 );
+      dogYears = 24 + ((humanYears-2) * 5 );
     }
-  }
   return [humanYears, catYears, dogYears];
 };
 
