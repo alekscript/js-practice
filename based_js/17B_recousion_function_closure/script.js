7803d4caad3b84a751913d9fcb9f3c9a04fb354b
alekscript <piatopal.alex@gmail.com>
2023-10-22 13:34:02 +0200

    add new lesson

--- based_js/17B_recousion_function_closure/script.js
+++ based_js/17B_recousion_function_closure/script.js
@@ -1,11 +1,48 @@
 "use strict";
 
-let number = 5;
+// let number = 5; debugger
 
-function logNumber() {
-    console.log(number);
+// function logNumber() {
+//     let number = 4; debugger
+//     console.log(number);
+// }
+
+// number = 6;
+
+// logNumber(); debugger
+
+
+function createCounter() {
+    let counter = 0;
+
+    const myFunc = function() {
+        counter = counter + 1;
+        return counter;
+    }
+
+    return myFunc;
 }
 
-number = 6;
+const increment = createCounter();
 
-logNumber();
+const c1 = increment();
+const c2 = increment();
+const c3 = increment();
+
+console.log(c1,c2,c3);
+
+// Замыкание функции - это функция, которая запоминает свои внешние переменные
+
+{
+    let msg = 'Hello';
+}
+
+console.log(msg); // ошибка
+
+for (let i = 0; i < 9; i++) {
+    for (let j = 0; j < 9; j++) {
+        let num = 3;
+    }
+    
+    console.log(num);
+}
